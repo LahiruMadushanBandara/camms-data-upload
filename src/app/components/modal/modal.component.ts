622ad48defcc2379a95051b3bc4b2211f5be70df
LahiruMadushanBandara <lahiru13bandara@gmail.com>
2022-08-03 09:39:23 +0530

    changes to modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { SharedService } from 'src/app/services/shared.service';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-modal',
@@ -8,20 +9,25 @@ import { SharedService } from 'src/app/services/shared.service';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  @ViewChild("home")
+  public home!: HomeComponent;
 
   constructor(private eventEmitterService: SharedService ){
   }
+
+
   
      public opened = false;
+     public nodeUploadOpened = false;
 
     public close(status: string): void {
         console.log(`Dialog result: ${status}`);
         this.opened = false;
     }
 
-    public open(isOpen:boolean): void {
+    public open(e:any): void {
       alert("dialog call")
-        this.opened = isOpen;
+        this.opened = true;
     }
 
     ngOnInit(): void {
