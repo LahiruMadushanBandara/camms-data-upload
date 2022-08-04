a9d118fc2009eac6563673f9907df0cd6475ee15
LahiruMadushanBandara <lahiru13bandara@gmail.com>
2022-08-05 00:32:48 +0530

    Custom error model

---
+++ src/app/models/CustomErrorModal.modal.ts
@@ -0,0 +1,8 @@
+export class CustomErrorModal {
+    RowNo?: string='';
+    Cell?: string='';
+    CellValue?: string='';
+    Column?: string='';
+    Message?: string='';
+    ExpectedDataType?: string='';
+}
