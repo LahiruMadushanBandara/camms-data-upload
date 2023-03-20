4809101e9bbf87d35b06755e8372a80f3bd6ce04
lahiru <lahiru@theavo.net>
2023-03-20 20:05:28 +0530

    config for api endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,15 +2,17 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import UrlConfig from 'src/assets/config/api-endpoints.json';
+
 export const environment = {
-  production: false
+  production: false,
+
+  //API Endpoints
+  HierarchyUrl : UrlConfig.HierarchyUrl,
+  HierarchyNodeUrl : UrlConfig.HierarchyNodeUrl,
+  FlexOrgStaff: UrlConfig.FlexOrgStaff,
+  FlexHierarchyAddStaffBulkUrl : UrlConfig.FlexHierarchyAddStaffBulkUrl,
+  getUserUrls : UrlConfig.getUserUrls
+
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
