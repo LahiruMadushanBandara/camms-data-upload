3a60433eb726b993f7c13ab5235dc36cd53301c5
lahiru <lahiru@theavo.net>
2023-03-27 21:51:05 +0530

    prod endpoint urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+import UrlConfig from 'src/assets/config/api-endpoints.json';
+
 export const environment = {
-  production: true
+  production: true,
+
+  //API Endpoints
+  HierarchyUrl : UrlConfig.HierarchyUrl,
+  HierarchyNodeUrl : UrlConfig.HierarchyNodeUrl,
+  FlexOrgStaff: UrlConfig.FlexOrgStaff,
+  FlexHierarchyAddStaffBulkUrl : UrlConfig.FlexHierarchyAddStaffBulkUrl,
+  getUserUrls : UrlConfig.getUserUrls
 };
