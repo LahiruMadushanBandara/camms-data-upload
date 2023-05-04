4d3701309b601d6b80ba2e9b6caf2802a114d974
Pasindu-Lakmal <pasindu1lak@gmail.com>
2023-05-04 12:45:51 +0530

    fixAuthTokenFieldView

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,30 +1,35 @@
-import { Component, EventEmitter, Input, Output, ViewChild, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class ModalComponent {
-  @ViewChild("home")
+  @ViewChild('home')
   public home!: HomeComponent;
 
   @Output() windowClose = new EventEmitter<boolean>();
   @Input() windowTitle = '';
 
-  
-  constructor(){
+  constructor() {}
+  public opened = false;
+  public nodeUploadOpened = false;
+
+  public close(status: boolean): void {
+    this.windowClose.emit(status);
   }
-     public opened = false;
-     public nodeUploadOpened = false;
 
-    public close(status: boolean): void {
-        this.windowClose.emit(status);
-    }
-
-    public open(e:any): void {
-      this.opened = true;
-    }
+  public open(e: any): void {
+    this.opened = true;
+  }
 }
