a06d99c6df3b139f87f1c535930eb91e8d1f129a
Pasindu-Lakmal <pasindu1lak@gmail.com>
2023-07-07 08:38:42 +0530

    addTable

--- src/app/components/common/upload-details-table/upload-details-table.component.ts
+++ src/app/components/common/upload-details-table/upload-details-table.component.ts
@@ -3,13 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-upload-details-table',
   templateUrl: './upload-details-table.component.html',
-  styleUrls: ['./upload-details-table.component.css']
+  styleUrls: ['./upload-details-table.component.css'],
 })
 export class UploadDetailsTableComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  public gridData: any[] = [
+    {
+      ProductID: 1,
+      ProductName: 'Chai',
+      UnitPrice: 18,
+      Category: {
+        CategoryID: 1,
+        CategoryName: 'Beverages',
+      },
+    },
+    {
+      ProductID: 2,
+      ProductName: 'Chang',
+      UnitPrice: 19,
+      Category: {
+        CategoryID: 1,
+        CategoryName: 'Beverages',
+      },
+    },
+    {
+      ProductID: 3,
+      ProductName: 'Aniseed Syrup',
+      UnitPrice: 10,
+      Category: {
+        CategoryID: 2,
+        CategoryName: 'Condiments',
+      },
+    },
+  ];
 }
